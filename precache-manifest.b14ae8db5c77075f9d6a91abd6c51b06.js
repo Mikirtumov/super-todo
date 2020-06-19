0167e889ba967a1442592ae547766bee0d0ffc13
SERGO_MIKIRTUMOV <mikirtumov@gmail.com>
2020-06-19 02:58:13 -0700

    Updates

---
+++ precache-manifest.b14ae8db5c77075f9d6a91abd6c51b06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d63c4fd98a4d895cd077b36704aaf39",
+    "url": "/super-todo/index.html"
+  },
+  {
+    "revision": "8ab53334b8cf92a1baa8",
+    "url": "/super-todo/static/css/main.cb7c532d.chunk.css"
+  },
+  {
+    "revision": "940761e91ae0c82c2f1d",
+    "url": "/super-todo/static/js/2.bf3e7c88.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/super-todo/static/js/2.bf3e7c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab53334b8cf92a1baa8",
+    "url": "/super-todo/static/js/main.635c4920.chunk.js"
+  },
+  {
+    "revision": "9e1032d13668df888e16",
+    "url": "/super-todo/static/js/runtime-main.268c1ae2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/super-todo/precache-manifest.fc83499c5ce8196349e861c5d674774d.js"
+  "/super-todo/precache-manifest.b14ae8db5c77075f9d6a91abd6c51b06.js"
 );
 
 self.addEventListener('message', (event) => {
