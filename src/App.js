ff55888e2c382fe2964c20849a6570c561ff8b4f
SERGO_MIKIRTUMOV <mikirtumov@gmail.com>
2020-06-20 23:43:54 -0700

    added list component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
+import List from "./components/List";
 
 
 function App() {
-  return (
-      <div className="todo">
-        <div className="todo__sidebar">
-          <button>Hello</button>
+    return (
+        <div className="todo">
+            <div className="todo__sidebar">
+                <List/>
+            </div>
+            <div className="todo__task"></div>
         </div>
-
-
-        <div className="todoTask"></div>
-
-      </div>
-  );
+    );
 }
 
 export default App;
