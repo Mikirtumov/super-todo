5432afb1b036ad3e63b090bc76aca3576730e2b2
SERGO_MIKIRTUMOV <mikirtumov@gmail.com>
2020-06-22 03:45:40 -0700

    Updates

--- precache-manifest.b91b6334ad176d43fb8acf499a59a1f5.js
+++ precache-manifest.b91b6334ad176d43fb8acf499a59a1f5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "67ea5cc10c95e72326dbc342053f4b40",
+    "revision": "2ad5370a115f00f360b71cb83b3ee438",
     "url": "/super-todo/index.html"
   },
   {
-    "revision": "9cd54271893260768a6d",
+    "revision": "40b61970264e768284fa",
     "url": "/super-todo/static/css/main.cb7c532d.chunk.css"
   },
   {
-    "revision": "b645c3a112e72088f45e",
-    "url": "/super-todo/static/js/2.466b66c0.chunk.js"
+    "revision": "53a0fa1ed2616dd2e495",
+    "url": "/super-todo/static/js/2.29ce73a0.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/super-todo/static/js/2.466b66c0.chunk.js.LICENSE.txt"
+    "url": "/super-todo/static/js/2.29ce73a0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9cd54271893260768a6d",
-    "url": "/super-todo/static/js/main.72e580db.chunk.js"
+    "revision": "40b61970264e768284fa",
+    "url": "/super-todo/static/js/main.e17a2c8f.chunk.js"
   },
   {
     "revision": "9e1032d13668df888e16",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/super-todo/precache-manifest.b54005549a56f9bc27d4862bdfac418d.js"
+  "/super-todo/precache-manifest.b91b6334ad176d43fb8acf499a59a1f5.js"
 );
 
 self.addEventListener('message', (event) => {
